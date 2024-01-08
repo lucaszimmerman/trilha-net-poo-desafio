7a6880ffda6695d0ae9b58812442f82c65e19e33
lucaszimmerman <lucaszimmermants@gmail.com>
2024-01-08 20:44:33 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("980843591", "Nokia Tijolão", "86153603019", 128 );
+Iphone i1 = new Iphone("980843592", "Iphone 15", "861536030196", 900 );
+i1.Ligar();
+n1.Ligar();
+n1.ReceberLigacao();
+i1.ReceberLigacao();
+n1.InstalarAplicativo("Subway Surfers");
+i1.InstalarAplicativo("Facebook");
